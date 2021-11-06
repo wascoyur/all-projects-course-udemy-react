79809d8ad8d90ae0621e306c26b08c9cf1463165
hp3007 <mytem20007@gmail.om>
2021-11-06 22:50:20 +0500

    implement statusViewer

--- components/todo/ListItem.jsx
+++ components/todo/ListItem.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import styles from "@/styles/ListItem.module.css";
 import { Button } from "react-bootstrap";
+import StatusViewer from "./status";
 
 export const ListItem = ({ item }) => {
-  const { name, important, status, changeStatus } = item;
+  const { id, name, important, status, changeStatus } = item;
 
   const [statusT, setStatusT] = useState(styles.normal);
 
@@ -13,10 +14,14 @@ export const ListItem = ({ item }) => {
     setStatusT(styles.done);
   };
 
-  console.log("status", status);
+  
   return (
-    <div className={() => changeStatus(id)}>
-      <span onClick={setActive}>
+    <div className={classNames}>
+      <span
+        onClick={() => {
+          changeStatus(id);
+        }}
+      >
         {name}/{status}
       </span>
       <Button variant="outline-danger" className="float-end ms-1">
@@ -25,9 +30,7 @@ export const ListItem = ({ item }) => {
       <Button variant="outline-success" className="float-end ms-1">
         <i className="bi bi-exclamation"></i>
       </Button>
-      <Button variant="outline-info" className="ms-1 float-end">
-        <i className="bi bi-pause-circle" />
-      </Button>
+      <StatusViewer statusTask={status} />
     </div>
   );
 };
