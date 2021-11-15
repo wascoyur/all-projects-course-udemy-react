d8ce8a9f69f7332c0c7cbf9777fcbe8d62bde3b9
wascoyur <mytem20007@gmail.com>
2021-11-15 17:38:02 +0500

    изменен фон

--- components/stardb/view/ItemDetailed.jsx
+++ components/stardb/view/ItemDetailed.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import Image from "next/image";
 
-const ItemDetailed = () => {
+const ItemDetailed = ({ activeItem }) => {
   return (
-    <div className="col">
-      <div className="card">
-        <Image src="/event-default.png" alt="?" width={100} height={100} />
-        <div className="card-body ">
-          <div className="list-group-flush">
-            <div className="list-group-item">cardd caption</div>
-            <div className="list-group-item">afdsvbtgr</div>
-            <div className="list-group-item">afdsvbtgr</div>
-          </div>
+    <div className="card bg-secondary">
+      <Image src="/event-default.png" alt="?" width={100} height={100} />
+      <div className="card-body bg-secondary">
+        <div className="list-group-flush ">
+          <div className="list-group-item bg-secondary">cardd caption</div>
+          <div className="list-group-item bg-secondary">afdsvbtgr</div>
+          <div className="list-group-item bg-secondary">afdsvbtgr</div>
         </div>
       </div>
     </div>
